da586262d6ec3a7513b764e2244591b7424677f8
DeepanshuShukla-2s16 <deepanshushukla571@gmail.comm>
2026-05-20 00:59:22 +0530

    Implemented dependency injection using @Autowired annotation

---
+++ src/main/java/com/deepanshu/spring_core_learning/annotations/autowired/NotificationService.java
@@ -0,0 +1,12 @@
+package com.deepanshu.spring_core_learning.annotations.autowired;
+
+/*
+ * Dependency class for notification handling.
+ */
+public class NotificationService {
+
+    public void sendNotification() {
+
+        System.out.println("Notification sent to customer.");
+    }
+}
